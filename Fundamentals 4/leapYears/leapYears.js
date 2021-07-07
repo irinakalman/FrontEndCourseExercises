89b384176136ae2283b1671449de3ebfa42e85c0
irinakalman <kalman0292@gmail.com>
2021-07-08 02:34:07 +0300

    solution for leapYears.js

--- Fundamentals 4/leapYears/leapYears.js
+++ Fundamentals 4/leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(num) {
 
+if ( (num % 4 == 0 && num % 100 !== 0) || (num % 400 == 0) ) {
+return true;
+} else if (num % 100 == 0){
+    return false;
+}else {
+    return false;
+}
 }
 
 module.exports = leapYears
