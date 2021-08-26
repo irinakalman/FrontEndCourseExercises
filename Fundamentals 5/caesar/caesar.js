4debf6d942586949985fb1c470485e150308b1e6
irinakalman <kalman0292@gmail.com>
2021-08-26 23:23:14 +0300

    solution for caesar exercise

--- Fundamentals 5/caesar/caesar.js
+++ Fundamentals 5/caesar/caesar.js
@@ -1,5 +1,28 @@
-const caesar = function() {
-
+let caesar = function(str, shift) {
+    let res = '';
+    for (let char of str) {
+        let code = char.charCodeAt();
+        let cipher = code;
+        if (code >= 65 && code <= 90) {
+            if (code + shift > 90) {
+                cipher = 64 + ((code + shift % 26) % 90);
+            } else if(code + shift < 65) {
+                cipher = 90 - (64 - (code + shift));
+            } else {
+                cipher = code + shift;
+            }
+        } else if (code >= 97 && code <= 122) {
+            if (code + shift > 122) {
+                cipher = 96 + ((code + shift % 26) % 122);
+            } else if (code + shift < 97) {
+                cipher = 122 - (96 - (code + shift));
+            } else {
+                cipher = code + shift;
+            }
+        }
+        res += String.fromCharCode(cipher);
+    }
+    return res;
 }
 
 module.exports = caesar
